92ec893590e31ba35ba06d8102444e07828c4d29
Anuj Decoded <nakuldhule301@gmail.com>
2026-05-25 13:51:16 +0530

    All code files pushed

---
+++ .cmd
@@ -0,0 +1,7 @@
+echo "# Anuj.Dev" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Codewithnikku/Anuj.Dev.git
+git push -u origin main
